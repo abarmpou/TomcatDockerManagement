e369fc2f25907e953f28fbe0d78e0020d3bab94f
Angelos Barmpoutis <131296747+abarmpou@users.noreply.github.com>
2024-01-10 23:17:40 -0500

    Create remove_tomcat.sh

---
+++ remove_tomcat.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Check if site_name is provided as an argument
+if [ -z "$1" ]; then
+    echo "Please provide the site_name as an argument."
+    exit 1
+fi
+
+site_name="$1"
+
+docker stop "$site_name"_tomcat
+docker rm "$site_name"_tomcat
